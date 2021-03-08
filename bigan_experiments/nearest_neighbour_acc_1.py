f9a83dea947fa4bb50e694348151c3497a009f43
Prabhat Agarwal <prabhat.agr2010@gmail.com>
2021-03-08 00:37:57 -0800

    Add comments

--- bigan_experiments/nearest_neighbour_acc_1.py
+++ bigan_experiments/nearest_neighbour_acc_1.py
@@ -7,63 +7,55 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 join = os.path.join
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', required=True,
-                    help='cifar10 | svhn | timagenet | cifar_mnist_cifar | cifar_mnist_mnist')
-parser.add_argument('--feat_dir', default="feats", help='path to dataset')
+if __name__ == '__main__':
 
-args = parser.parse_args()
 
-dataset = args.dataset
-feat_dir = args.feat_dir
-# labels_dir = feat_dir
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', required=True,
+                        help='cifar10 | svhn | timagenet | cifar_mnist_cifar | cifar_mnist_mnist')
+    parser.add_argument('--feat_dir', default="feats", help='path to dataset')
 
-labels_dir = args.feat_dir
+    args = parser.parse_args()
 
-# train_features = np.load(join(feat_dir, "train_feats_ae_5_moco_load.npy"))
-# val_features = np.load(join(feat_dir, "val_feats_ae_5_moco_load.npy"))
+    dataset = args.dataset
+    feat_dir = args.feat_dir
+    labels_dir = args.feat_dir
 
-# train_labels = np.load(join(feat_dir, "train_labels_ae_5_moco_load.npy"))
-# val_labels = np.load(join(feat_dir, "val_labels_ae_5_moco_load.npy"))
+    # Load train and test features
+    train_features = np.load(join(feat_dir, dataset + "_train_feats.npy"))
+    val_features = np.load(join(feat_dir, dataset + "_test_feats.npy"))
 
-train_features = np.load(join(feat_dir, dataset + "_train_feats.npy"))
-val_features = np.load(join(feat_dir, dataset + "_test_feats.npy"))
 
-# train_features = np.load(join(feat_dir, "train_feats_ae_6_moco.npy"))
-# val_features = np.load(join(feat_dir, "val_feats_ae_6_moco.npy"))
+    train_labels = np.load(join(labels_dir, dataset + "_train_labels.npy"))
+    val_labels = np.load(join(labels_dir, dataset + "_test_labels.npy"))
 
-# train_features = np.load(join(feat_dir, "moco_nvae_1_train_feats_intriguing.npy"))
-# val_features = np.load(join(feat_dir, "moco_nvae_1_val_feats_intriguing.npy"))
+    train_features = np.reshape(train_features, [len(train_features), -1])
+    val_features = np.reshape(val_features, [len(val_features), -1])
 
-train_labels = np.load(join(labels_dir, dataset + "_train_labels.npy"))
-val_labels = np.load(join(labels_dir, dataset + "_test_labels.npy"))
+    print(train_features.shape, val_features.shape, train_labels.shape, val_labels.shape)
+    indices = np.arange(len(train_features))
+    np.random.shuffle(indices)
 
-train_features = np.reshape(train_features, [len(train_features), -1])
-val_features = np.reshape(val_features, [len(val_features), -1])
+    top_k = 100
 
-print(train_features.shape, val_features.shape, train_labels.shape, val_labels.shape)
-indices = np.arange(len(train_features))
-np.random.shuffle(indices)
+    indices = indices[:50000]
+    train_features = train_features[indices]
+    train_labels = train_labels[indices]
 
-top_k = 100
+    # distances = euclidean_distances(val_features, train_features)
+    distances = -cosine_similarity(val_features, train_features)
 
-indices = indices[:50000]
-train_features = train_features[indices]
-train_labels = train_labels[indices]
+    pred_indices = np.argsort(distances, axis=-1)[:, :top_k]
 
-# distances = euclidean_distances(val_features, train_features)
-distances = -cosine_similarity(val_features, train_features)
+    pred_labels = train_labels[pred_indices]
 
-pred_indices = np.argsort(distances, axis=-1)[:, :top_k]
+    # Generate classification accuracy for different values of k from 1 to 100
+    top_k = np.arange(0, 101, 5)
+    top_k[0] = 1
+    for k in top_k:
+        out = stats.mode(pred_labels[:, :k], axis=1).mode
 
-pred_labels = train_labels[pred_indices]
+        labels = np.array([val[0] for val in out])
 
-top_k = np.arange(0, 101, 5)
-top_k[0] = 1
-for k in top_k:
-    out = stats.mode(pred_labels[:, :k], axis=1).mode
-
-    labels = np.array([val[0] for val in out])
-
-    # print(pred_labels.shape, val_labels.shape)
-    print(k, np.mean(np.equal(labels, val_labels)))
+        # print(pred_labels.shape, val_labels.shape)
+        print(k, np.mean(np.equal(labels, val_labels)))
